0cb160cb188db760622ee027dac673323f732649
K413l <kalel.schlichting@gmail.com>
2024-09-13 20:30:59 -0300

    CRUD CRIADO NO BANCO ORACLE

--- App.py
+++ App.py
@@ -5,21 +5,56 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+cx_oracle://RM551007:030803@oracle.fiap.com.br:1521/orcl'
 db = SQLAlchemy(app)
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    new_user = User(name=data['name'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"message": "Usuário criado com sucesso!"}), 201
+# Modelo para a tabela BANCO
+class Banco(db.Model):
+    __tablename__ = 'BANCO'
+    id_banco = db.Column('ID_BANCO', db.Integer, primary_key=True)
+    nm_banco = db.Column('NM_BANCO', db.String(100), nullable=True)
+    cd_banco = db.Column('CD_BANCO', db.String(10), nullable=True)
 
-@app.route('/users', methods=['GET'])
-def create_user():
+db.create_all()
+
+@app.route('/bancos', methods=['POST'])
+def create_banco():
     data = request.get_json()
-    new_user = User(name=data['name'])
-    db.session.add(new_user)
+    new_banco = Banco(nm_banco=data['nm_banco'], cd_banco=data['cd_banco'])
+    db.session.add(new_banco)
     db.session.commit()
-    return jsonify({"message": "Usuário criado com sucesso!"}), 201    
+    return jsonify({"message": "Banco criado com sucesso!"}), 201
+
+@app.route('/bancos', methods=['GET'])
+def get_bancos():
+    bancos = Banco.query.all()
+    return jsonify([{'id_banco': banco.id_banco, 'nm_banco': banco.nm_banco, 'cd_banco': banco.cd_banco} for banco in bancos])
+
+@app.route('/bancos/<int:id>', methods=['GET'])
+def get_banco(id):
+    banco = Banco.query.get(id)
+    if not banco:
+        return jsonify({"message": "Banco não encontrado"}), 404
+    return jsonify({'id_banco': banco.id_banco, 'nm_banco': banco.nm_banco, 'cd_banco': banco.cd_banco})
+
+@app.route('/bancos/<int:id>', methods=['PUT'])
+def update_banco(id):
+    data = request.get_json()
+    banco = Banco.query.get(id)
+    if not banco:
+        return jsonify({"message": "Banco não encontrado"}), 404
+
+    banco.nm_banco = data['nm_banco']
+    banco.cd_banco = data['cd_banco']
+    db.session.commit()
+    return jsonify({"message": "Banco atualizado com sucesso!"})
+
+@app.route('/bancos/<int:id>', methods=['DELETE'])
+def delete_banco(id):
+    banco = Banco.query.get(id)
+    if not banco:
+        return jsonify({"message": "Banco não encontrado"}), 404
+
+    db.session.delete(banco)
+    db.session.commit()
+    return jsonify({"message": "Banco excluído com sucesso!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
