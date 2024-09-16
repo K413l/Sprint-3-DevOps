e82283d1696689f3ed695e2be0ce6ce482f6e08f
K413l <kalel.schlichting@gmail.com>
2024-09-16 19:35:14 -0300

    update requirements e mudança versão python

--- App.py
+++ App.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 # Utilize a variável de ambiente definida no Azure
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "oracle+oracledb://RM551007:030803@oracle.fiap.com.br:1521/orcl"
 
 db = SQLAlchemy(app)
 
